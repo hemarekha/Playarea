5fea354db55b84aafb6c57a9a77c58a167824b01
hemarekha <45401020+hemarekha@users.noreply.github.com>
2020-09-22 15:26:10 +0530

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+export JAVA_HOME=/Library/Java/Home
+export M2_HOME=/Volumes/Karthik/Devops/softwares/apache-maven-3.6.3
+export M2=$M2_HOME/bin
 set -x
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 set +x
